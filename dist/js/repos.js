e397016d0c3c5b5baff089d6d95fc84742f01bbe
p7m <perpetuum7@protonmail.com>
2022-05-29 20:58:40 +0200

    feat: add sdk repo

--- dist/js/repos.js
+++ dist/js/repos.js
@@ -4,7 +4,11 @@ const koyoRepos = [
     name: "koyo",
   },
   {
+    full_name: "koyo-finance/sdk",
+    name: "sdks",
+  },
+  {
     full_name: "koyo-finance/exchange-info",
-    name: "exchange-info"
-  }
+    name: "exchange-info",
+  },
 ];
