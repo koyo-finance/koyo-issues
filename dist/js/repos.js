bce8caf5e319c2067ae68933823cc2c73da7b59c
p7m <perpetuum7@protonmail.com>
2022-06-11 12:26:49 +0200

    feat: include more repos in issues

--- dist/js/repos.js
+++ dist/js/repos.js
@@ -12,7 +12,15 @@ const koyoRepos = [
     name: "exchange-contracts",
   },
   {
+    full_name: "koyo-finance/exchange-interface",
+    name: "exchange-interface",
+  },
+  {
     full_name: "koyo-finance/exchange-info",
     name: "exchange-info",
+  },
+  {
+    full_name: "koyo-finance/docs",
+    name: "docs",
   },
 ];
